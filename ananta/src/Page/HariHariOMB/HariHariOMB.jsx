4c220338bfb7cb76b8a6adfa8d42d43e79ac92fd
Aldrik56 <aldrik.ciaputra@student.umn.ac.id>
2023-08-22 02:25:17 +0700

    update responsive

--- ananta/src/Page/HariHariOMB/HariHariOMB.jsx
+++ ananta/src/Page/HariHariOMB/HariHariOMB.jsx
@@ -3,6 +3,7 @@ import './hariHariOMB.scss';
 import hariPenetasan from './hariPenetasan.js';
 import hariPembentukan1 from './hariPembentukan1';
 import hariPembentukan2 from './hariPembentukan2';
+import TopInfo from "../../Components/TopInfo/TopInfo";
 const HariHariOMB= (props)=> {
     const [day,setDay] = useState(props.id == null ? 0 : props.id);
     const array = [
@@ -26,36 +27,49 @@ const HariHariOMB= (props)=> {
     ] ;
 
     return (
-        <div className="hariHariOMB__section">
-            <div className="bg1" alt="" />
-            <div  className="bg2" alt="" />
-            <div className="hariHari__titleBox">
-                <h1 className="hariHariOMB__title">{array[day].title}</h1>
-                <p className="hariHariOMB__subTitle">{array[day].subTitle}</p>
-            </div>
-            <div className="hariHariOMB__aksesBox">
-                <div>
-                    <p>Mau lihat semua dokumentasi?</p>
-                    <div className="aksesDriveDiv">
-                        <img src="./Assets/HariHariOMB/downloadDrive.svg" alt="" />
-                        <a href="">Akses Drive</a>
+        <div className='bg_container1'>
+            <div className="bg_container2">
+                <div className="hariHariOMB__section">
+                    <TopInfo />
+                    <div className="hariHari__titleBox">
+                        <h1 className="hariHariOMB__title">{array[day].title}</h1>
+                        <p className="hariHariOMB__subTitle">{array[day].subTitle}</p>
+                    </div>
+                    <div className="hariHariOMB__aksesBox">
+                        <div>
+                            <p>Mau lihat semua dokumentasi?</p>
+                            <div className="aksesDriveDiv">
+                                <img src="./Assets/HariHariOMB/downloadDrive.svg" alt="" />
+                                <a href="">Akses Drive</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="hariHariOMB__photoGrid">
+                        <img src={array[day].asset[0].img} class="photo1" alt="" />
+                        <img src={array[day].asset[1].img} class="photo2" alt="" />
+                        <img src={array[day].asset[2].img} class="photo3" alt="" />
+                        <img src={array[day].asset[3].img} class="photo4" alt="" />
+                        <img src={array[day].asset[4].img} class="photo5" alt="" />
+                        <img src={array[day].asset[5].img} class="photo6" alt="" />
+                        <img src={array[day].asset[6].img} class="photo7" alt="" />
+                        <img src={array[day].asset[7].img} class="photo8" alt="" />
+                        <img src={array[day].asset[8].img} class="photo9" alt="" />
+                        <img src={array[day].asset[9].img} class="photo10" alt="" />
+                        <img src={array[day].asset[10].img} class="photo11" alt="" />
+                        <img src={array[day].asset[10].img} class="photo12" alt="" />
+                        <img src={array[day].asset[10].img} class="photo13" alt="" />
+                        <img src={array[day].asset[10].img} class="photo14" alt="" />
+                        <img src={array[day].asset[10].img} class="photo15" alt="" />
+                        <img src={array[day].asset[10].img} class="photo16" alt="" />
+                        <img src={array[day].asset[10].img} class="photo17" alt="" />
+                        <img src={array[day].asset[10].img} class="photo18" alt="" />
+                        <img src={array[day].asset[10].img} class="photo19" alt="" />
+                        <img src={array[day].asset[10].img} class="photo20" alt="" />
                     </div>
                 </div>
             </div>
-            <div className="hariHariOMB__photoGrid">
-                <img src={array[day].asset[0].img} class="photo1" alt="" />
-                <img src={array[day].asset[1].img} class="photo2" alt="" />
-                <img src={array[day].asset[2].img} class="photo3" alt="" />
-                <img src={array[day].asset[3].img} class="photo4" alt="" />
-                <img src={array[day].asset[4].img} class="photo5" alt="" />
-                <img src={array[day].asset[5].img} class="photo6" alt="" />
-                <img src={array[day].asset[6].img} class="photo7" alt="" />
-                <img src={array[day].asset[7].img} class="photo8" alt="" />
-                <img src={array[day].asset[8].img} class="photo9" alt="" />
-                <img src={array[day].asset[9].img} class="photo10" alt="" />
-                <img src={array[day].asset[10].img} class="photo11" alt="" />
-            </div>
-        </div> 
+        </div>
+            
     );
 }
 
