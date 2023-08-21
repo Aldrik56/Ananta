a48762562c57a78f6404bfd0b4afce252dff1e9f
Aldrik56 <aldrik.ciaputra@student.umn.ac.id>
2023-08-22 00:06:36 +0700

    aftermath revert changes
    nambahin hariHariOMB sama masukin update terakhir daffa

--- ananta/src/Page/index.js
+++ ananta/src/Page/index.js
@@ -1,3 +1,4 @@
 export { FotoDivisi } from "./FotoDivisi";
 export { HariPelaksanaan } from "./HariPelaksanaan";
 export { AfterMovie } from "./AfterMovie";
+export { HariHariOMB } from "./HariHariOMB";
