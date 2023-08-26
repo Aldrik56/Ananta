d5040ac52498bd40732d1399fc36c10df52b3741
KoongHiap <98584656+Koongh@users.noreply.github.com>
2023-08-26 22:48:57 +0700

    fix: particle with clock and firefly particle

--- ananta/src/Components/ParticleKupu_Kupu/ParticleConfig.js
+++ ananta/src/Components/ParticleKupu_Kupu/ParticleConfig.js
@@ -7,27 +7,54 @@ export const ParticleConfig = {
         value_area: 800,
       },
     },
+    color: "#f1ed89",
     shape: {
       type: "image",
       image: [
         {
-          src: "https://cdn.discordapp.com/attachments/1133328017410764891/1144940526639001652/kupu_3.png",
+          src: "https://cdn.discordapp.com/attachments/1133328017410764891/1145017842073338049/Group.png",
           width: 100,
           height: 100,
         },
+        // {
+        //   src: "https://cdn.discordapp.com/attachments/1133328017410764891/1144940526639001652/kupu_3.png",
+        //   width: 100,
+        //   height: 100,
+        // },
+        // {
+        //   src: "https://cdn.discordapp.com/attachments/1133328017410764891/1144940527112945674/kupu_2.png",
+        //   width: 100,
+        //   height: 100,
+        // },
+        // {
+        //   src: "https://cdn.discordapp.com/attachments/1133328017410764891/1144940527515607101/kupu_4.png",
+        //   width: 100,
+        //   height: 100,
+        // },
+        // {
+        //   src: "https://cdn.discordapp.com/attachments/1133328017410764891/1145007984171294801/kupu_1_1.png",
+        //   width: 100,
+        //   height: 100,
+        // },
+        // {
+        //   src: "https://cdn.discordapp.com/attachments/1133328017410764891/1145009606406443168/graph.png",
+        //   width: 100,
+        //   height: 100,
+        // },
+        // {
+        //   src: "https://media.tenor.com/-Y2YOay3_JoAAAAM/its-friday-dancing.gif",
+        //   width: 100,
+        //   height: 100,
+        // },
+        // {
+        //   src: "https://cdn.discordapp.com/attachments/1133328017410764891/1145016682037923951/Ellipse_1.png",
+        //   width: 100,
+        //   height: 100,
+        // },
+
         {
-          src: "https://cdn.discordapp.com/attachments/1133328017410764891/1144940527112945674/kupu_2.png",
-          width: 100,
-          height: 100,
-        },
-        {
-          src: "https://cdn.discordapp.com/attachments/1133328017410764891/1144940527515607101/kupu_4.png",
-          width: 100,
-          height: 100,
-        },
-        {
-          src: "https://cdn.discordapp.com/attachments/1133328017410764891/1145007984171294801/kupu_1_1.png",
-          width: 100,
+          src: "https://cdn.discordapp.com/attachments/1133328017410764891/1145021655303856229/firefly.png",
+          width: 200,
           height: 100,
         },
       ],
